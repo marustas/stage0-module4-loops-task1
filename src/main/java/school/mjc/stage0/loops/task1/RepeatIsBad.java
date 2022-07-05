5b4cecf97ba8e23862299baefeed9aace238e710
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 17:45:10 +0300

    LAST TRY

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -1,7 +1,8 @@
 package school.mjc.stage0.loops.task1;
 
 public class RepeatIsBad {
-    public void repeatIsBad(){
+
+    public void repeatIsBad() {
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
@@ -23,4 +24,9 @@ public class RepeatIsBad {
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
     }
+
+    public static void main(String[] args) {
+        new RepeatIsBad().repeatIsBad();
+    }
 }
+
