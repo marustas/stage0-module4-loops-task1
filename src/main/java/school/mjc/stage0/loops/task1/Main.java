2523ce04d4208e0b2d52cdddc819fef623850c2c
Stanislau Marudau <marudov.stas@gmail.com>
2022-07-05 13:41:32 +0300

    Another try

--- src/main/java/school/mjc/stage0/loops/task1/Main.java
+++ src/main/java/school/mjc/stage0/loops/task1/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         new Main().repeatIsBad();
     }
-
     public void repeatIsBad() {
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
         System.out.println("writing the same code doesn't have much impact, and it's also time consuming");
